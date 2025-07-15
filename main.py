523d138a0fa953c450978891a6c81b5001495560
chandlerwilliams <chan513chan@yahoo.com>
2025-07-15 11:35:45 -0400

    first commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def in_autotests_we_trust(a, b):
     if a == b:
         print('Test passed')
     else:
-        print('Test failed')
+        print('Test-failed')
 
 in_autotests_we_trust(10, '10')
 
